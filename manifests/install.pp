2b827712ce6252987a457ba7a5610521d7a22b7d
Garrett Rowell <garrett@puppet.com>
2024-09-13 13:14:02 -0600

    try

--- manifests/install.pp
+++ manifests/install.pp
@@ -65,7 +65,7 @@ class chocolatey::install {
   #    require      => Registry_value['ChocolateyInstall environment value'],
   #  }
   exec { 'install_chocolatey_official':
-    command   => inline_epp("${epp('chocolatey/install_options.epp')}${epp('chocolatey/install.ps1.epp')}"),
+    command   => inline_epp("${epp('chocolatey/install_options.epp')}; & ${epp('chocolatey/install.ps1.epp')}"),
     creates   => "${chocolatey::choco_install_location}\\bin\\choco.exe",
     provider  => powershell,
     timeout   => $chocolatey::choco_install_timeout_seconds,
